9b4ad4d28aeaa059a543c4e6e77ea4a2c20e0ec1
Alex5000tm <154931151+Alex5000tm@users.noreply.github.com>
2024-01-06 15:17:30 +0500

    Add files via upload

---
+++ ACXfile002G.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  5 21:38:36 2024
+
+@author: Я
+"""
+
+import math
+a=22
+b=30
+c=70
+d=90
+i=0
+while (i<=100):
+    print (math.cos(a*b+i)/math.sin(c*d+i)*i**2)
+    i=i+1
+print ('End')
+input()
