f65a915b39a44e61c4fcb1bea1b2ddeaae6f12e4
Alex5000tm <154931151+Alex5000tm@users.noreply.github.com>
2024-01-06 13:49:13 +0500

    Add files via upload

---
+++ ACXfile001G.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  5 19:57:36 2024
+
+@author: Я
+"""
+from math import cos, sin
+a=30
+b=60
+c=-35
+for i in range (0,50,5):
+    print (cos (a)*cos(b)/sin(c)*i)
+print ('End')
+import time
+time.sleep(8)
