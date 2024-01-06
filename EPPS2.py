7c748d4aeba737ee60e6c766bbf356ca13c449e0
Alex5000tm <154931151+Alex5000tm@users.noreply.github.com>
2024-01-06 17:48:58 +0500

    Add files via upload

---
+++ EPPS2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  6 17:38:26 2024
+
+@author: Я
+"""
+
+print ('Math_1.1')
+a= float(input('a= '))
+b= float(input('b= '))
+i=0
+while (i<=11):
+    print (a*b*i)
+    i=i+1
+print ('_End_')
+input()
